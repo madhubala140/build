4d95b43dae5bd5219f829b55ca12a7382fcf0814
madhubala140 <madhusekar.s1994@gmail.com>
2024-02-01 16:05:17 +0530

    countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+const App = () => {
+	const [Countries, setCountries] = useState([]);
+	const [searchValue, setSearchValue] = useState("");
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const url = "https://restcountries.com/v3.1/all";
+				const res = await axios(url, {
+					method: "GET",
+				});
+				const data = await res.data;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+				setCountries(data);
+			} catch (err) {
+				console.error("Error fetching data: ", err);
+			}
+		};
+		fetchData();
+	});
+
+	return (
+		<>
+			<nav>
+				<input
+					type="text"
+					id="search"
+					value={searchValue}
+					placeholder="Search for countries..."
+					onChange={(e) => {
+						setSearchValue(e.target.value);
+					}}
+				/>
+			</nav>
+			<div className="container">
+				{Countries?.map((ele) => {
+					const name = ele.name.common;
+					if (
+						name.toLowerCase().includes(searchValue.toLocaleLowerCase()) ||
+						searchValue === ""
+					) {
+						return (
+							<div key={ele.cca3} className="countryCard">
+								<img src={ele.flags.png} alt={`Flag of ${ele.name.common}`} />
+								<h2>{ele.name.common}</h2>
+							</div>
+						);
+					}
+				})}
+			</div>
+		</>
+	);
+};
 
 export default App;
